99953a0e8941ea6040dc7f3203ff1fc05c397097
tp6fu6m3 <m3tp6fu6@gmail.com>
2021-04-23 11:12:03 +0800

    Add files via upload

---
+++ Ex5/src/EightQueen.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <stdio.h> 
+#include <stdlib.h> 
+#define N 8 
+using namespace std;
+
+bool column[N] = {0};
+bool slash[2 * N] = {0};
+bool backSlash[2 * N] = {0};
+int queens[N] = {0}; 
+ofstream outputFile;
+
+void printQ(){
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < N; j++){
+			if(queens[i] == j)
+				outputFile << "Q ";
+			else
+				outputFile << "- ";
+		}
+		outputFile << endl;
+	}
+	outputFile << endl;
+}
+
+void backTrack(int i){
+	if(i >= N)
+		printQ();
+	else{
+		for(int j = 0; j < N; j++){
+			if(!(column[j] || slash[i + j] || backSlash[i - j + N])){
+				queens[i] = j;
+				column[j] = slash[i + j] = backSlash[i - j + N] = true;
+				backTrack(i+1);
+				column[j] = slash[i + j] = backSlash[i - j + N] = false;
+			} 
+		}
+	}
+}
+
+int main(){
+	outputFile.open("../data/output.txt");
+	backTrack(0); 
+	outputFile.close();
+	return 0;
+} 
+
